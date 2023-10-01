8135de93e23d76414dc1ad9702150d2073aa8826
IT STEP <memasik4you@gmal.com>
2023-10-01 10:37:16 +0300

    ex3

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-numbers = input("Введіть елементи списку: ")
-number = input("Введіть число: ")
-count = numbers.count(number)
-print("Кількість входжень числа {0} в список: {1}".format(number, count))
+input_str = input("Введіть елементи списку цілих чисел: ")
+numbers = list(map(int, input_str.split()))
+total_sum = sum(numbers)
+average = total_sum / len(numbers)
+print(f"Сума всіх елементів: {total_sum}")
+print(f"Середнє арифметичне: {average}")
+
+
 
 
