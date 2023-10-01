a57a202238b9795f6db97134b8c7dc16256be76b
IT STEP <memasik4you@gmal.com>
2023-10-01 10:13:48 +0300

    ex2

---
+++ main.py
@@ -0,0 +1,6 @@
+numbers = input("Введіть елементи списку: ")
+number = input("Введіть число: ")
+count = numbers.count(number)
+print("Кількість входжень числа {0} в список: {1}".format(number, count))
+
+
